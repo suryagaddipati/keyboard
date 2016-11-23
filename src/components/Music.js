a182155487ec9e0e7d8e159d34fafe6a5b829b26
Surya Gaddipati <surya.gaddipati@gmail.com>
2016-11-23 12:05:35 -0600

    midiplus

--- src/components/Music.js
+++ src/components/Music.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Tone from 'tone';
+import WebMidi from 'WebMidi';
 var keypress = require( 'keypress.js')
 class Music extends React.Component {
   constructor(props) {
@@ -10,6 +11,62 @@ class Music extends React.Component {
     this.allowed = true
   }
   componentDidMount() {
+    var synth = this.synth;
+    WebMidi.enable(function (err) {
+
+      if (err) {
+        console.log("WebMidi could not be enabled.", err);
+      } else {
+        var input = WebMidi.getInputByName("SmartPAD");
+        input.addListener('noteon', "all", function(e) {
+          synth.triggerAttackRelease(e.note.name+e.note.octave, "8n");
+          console.log( e.note);
+        });
+        input.addListener('controlchange', "all", function(e) {
+          console.log( e.data);
+        });
+        input.addListener('channelmode', "all", function(e) {
+          console.log( e);
+        });
+        input.addListener('programchange', "all", function(e) {
+          console.log( e);
+        });
+        input.addListener('songposition', "all", function(e) {
+          console.log( e);
+        });
+        input.addListener('songselect', "all", function(e) {
+          console.log( e);
+        });
+        input.addListener('sysex', "all", function(e) {
+          console.log( e);
+        });
+        input.addListener('tuningrequest', "all", function(e) {
+          console.log( e);
+        });
+        input.addListener('unknownsystemmessage', "all", function(e) {
+          console.log( e);
+        });
+        input.addListener('timecode', "all", function(e) {
+          console.log( e);
+        });
+        input.addListener('reset', "all", function(e) {
+          console.log( e);
+        });
+
+        input.addListener('start', "all", function(e) {
+          console.log( e);
+        });
+        input.addListener('stop', "all", function(e) {
+          console.log( e);
+        });
+
+
+      }
+
+    });
+
+
+
     var listener = new keypress.keypress.Listener();
     listener.simple_combo("shift s", this.tabSpace);
 
